2301f1eeabbb8595e3004f2c67b12a517073049c
Dev-Pi-Ai <huff-ether-upriver@duck.com>
2025-08-25 13:31:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,24 @@ while True:
 
     elif 'restart' in command:
         os.system("restart")
+
+    elif 'help' in command:
+        print("""
+        1. Timer: a timer program (if only there was some was to infer the function of this program).
+        Usage: timer
+
+        2. Guesses: a simple hangman-like word guessing game.
+        DO NOT DELETE, REMOVE, OR EXECUTE THE "guesses_data.py" FILE IT IS USED BY THE GAME FOR A LIST OF WORDS.
+        Usage: guesses (guess letters in the word within ten guesses).
+        Tip: (you can add or remove words from the game by modifying the "guessing_data.py").
+
+        3. Basic Calculator: a very simple calculator (outdated)
+        Usage: basic_calculator (enter in a simpe equation +-*/% execpted i.e "2 + 2", "1 % 10"
+
+        4. Advanced Calculator: a much more complicated calculator capible of solving equations with multiple opperators with PEMDAS.
+        Usage: advanced_calculator (enter an equation (only use ONE pair of paranthesis per equation or you will have a error)
+        quit: to exit
+        clear: to clear the calculator
     
     else:
         os.system(f"cd plugins\npython3 {command}.py")
